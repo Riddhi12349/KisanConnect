7cf83e9e9ba68e2cc955c77ff5442b6817ed474b
riddhi <riddhigarg385@gmail.com>
2024-10-16 02:13:51 +0530

    commit

--- investment/investmentRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router();
-const investmentController = require('./investmentController')
-
-router.post('/newInvestment',investmentController.createInvestment)
-router.get('/getInvestmentsByUserId/:id',investmentController.getInvestmentsByUserId)
-
-module.exports = router;
